25f126feaa55b26b7a195e175d2d07a650a328ee
Luca Lewin <luca.lewin23@gmail.com>
2023-03-26 17:43:42 +0200

    implement proof of concept for color change

--- content.js
+++ content.js
@@ -173,6 +173,7 @@ const message_group_callback = (mutationList, observer) => {
     if (mutation.type === "childList") {
       for (let node of mutation.addedNodes) {
         if (node.tagName == "CIB-MESSAGE-GROUP") {
+          let messageGroup = node.shadowRoot;
           let stylesheet = document.createElement("style");
           stylesheet.innerHTML =
           `
@@ -180,7 +181,37 @@ const message_group_callback = (mutationList, observer) => {
               background-color: #333 !important;
             }
           `;
-          node.shadowRoot.appendChild(stylesheet);
+          messageGroup.appendChild(stylesheet);
+
+          // change color of text
+          // only if the message group is from the bot
+          new MutationObserver(cib_message_callback).observe(messageGroup, config);
+        }
+      }
+    }
+  }
+}
+
+function cib_message_callback(mutationList, observer) {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      for (let node of mutation.addedNodes) {
+        if (node.tagName == "CIB-MESSAGE") {
+          if (node.getAttribute("type") == "text") {
+            let cibMessage = node.shadowRoot;
+
+            setTimeout(function () {
+              let style = document.createElement("style");
+              style.innerHTML = '*:not(sup) { color: #dadada !important; }';
+
+              let textBlock = cibMessage.querySelector(".ac-textBlock");
+              textBlock.appendChild(style);
+
+              console.log(textBlock);
+            }, 4000);
+
+            observer.disconnect();
+          }
         }
       }
     }
